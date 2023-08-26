f1bfb94b8c4e1da93e24110ad3a7c26a426a1add
Khant-Nyar <gnext.khantsithuphyo@gmail.com>
2023-08-27 00:03:28 +0630

    add typeorm,configuration and first resources

---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: (ConfigService:ConfigService) => ({
+                type: `mysql`,
+                host: ConfigService.getOrThrow('MYSQL_HOST'),
+                port: ConfigService.getOrThrow('MYSQL_PORT'),
+                database: ConfigService.getOrThrow('MYSQL_DATABASE'),
+                username: ConfigService.getOrThrow('MYSQL_USER'),
+                password: ConfigService.getOrThrow('MYSQL_PASSWORD'),
+                synchronize: ConfigService.getOrThrow('MYSQL_SYNCHRONIZE'),
+                autoLoadEntities: true,
+            }),
+            inject: [ConfigService]
+        })
+    ]
+})
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log(process.env.APP_PORT);
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(5000); //APP_PORT
+    
+    app.setGlobalPrefix('api/v1');
+    await app.listen(process.env.APP_PORT || 5000);
 }
 bootstrap();
