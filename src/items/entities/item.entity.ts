3841435df97a8f35e444a4b95813e4a3bc4a22e2
Khant-Nyar <gnext.khantsithuphyo@gmail.com>
2023-08-27 00:54:04 +0630

    nest crud with Repository and EntityManager

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,19 @@
-export class Item {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Item {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+    constructor(item:Partial<Item>){
+        Object.assign(this,item)
+        // super();
+        // Object.assign(this, param);
+    }
+}
