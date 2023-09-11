9f2d736ee621f167fb83185e373bafe4fa85f704
Khant-Nyar <gnext.khantsithuphyo@gmail.com>
2023-09-11 15:47:10 +0630

    init new app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-// import { CustomInterceptor } from './custom.interceptor';
-import { ReportsService } from './reports/reports.service';
-import { ReportsModule } from './reports/reports.module';
-import { SummaryModule } from './summary/summary.module';
-import { SummaryService } from './summary/summary.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [ReportsModule, SummaryModule],
-  controllers: [],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: ClassSerializerInterceptor,
-    },
-    ReportsService,
-    SummaryService,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    abortOnError: false,
-  });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
-  await app.listen(5000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
